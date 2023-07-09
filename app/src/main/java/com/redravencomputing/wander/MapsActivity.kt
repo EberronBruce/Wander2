37e2fd8893ac6bceb8e382d9609b2f2ab8990a77
Bruce <bruce@redravencomputing.com>
2023-07-08 21:47:06 -0500

    Add user location and go to it.

--- app/src/main/java/com/redravencomputing/wander/MapsActivity.kt
+++ app/src/main/java/com/redravencomputing/wander/MapsActivity.kt
@@ -1,11 +1,17 @@
 package com.redravencomputing.wander
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.content.res.Resources
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -23,6 +29,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 	private lateinit var binding: ActivityMapsBinding
 	private lateinit var map: GoogleMap
 	private val TAG = MapsActivity::class.java.simpleName
+	private val REQUEST_LOCATION_PERMISSION = 1
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -54,6 +61,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 		setMapLongClick(map)
 		setPoiClick(map)
 		setMapStyle(map)
+		enableMyLocation()
 	}
 
 	private fun setMapLongClick(map: GoogleMap) {
@@ -100,6 +108,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 			}
 		} catch (e: Resources.NotFoundException) {
 			Log.e(TAG, "Can't find style. Error: ", e)
+		}
+	}
+
+	private fun isPermissionGranted() : Boolean {
+		return ActivityCompat.checkSelfPermission(
+			this,
+			Manifest.permission.ACCESS_FINE_LOCATION
+		) == PackageManager.PERMISSION_GRANTED
+	}
+
+	private fun enableMyLocation() {
+		if (isPermissionGranted()) {
+			map.setMyLocationEnabled(true)
+			// Get the last known location of the device
+			val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+			fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+				location?.let {
+					// Create a LatLng object with the current location coordinates
+					val currentLatLng = LatLng(location.latitude, location.longitude)
+
+					// Move the camera to the current location
+					map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+				}
+			}
+		}
+		else {
+			ActivityCompat.requestPermissions(
+				this,
+				arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+				REQUEST_LOCATION_PERMISSION
+			)
 		}
 	}
 
